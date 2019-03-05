8ee1a59fef8fa34496e16bec91a6bd157f807b24
hughdevore <hughdevore@gmail.com>
2019-03-05 14:10:05 -0700

    CI-4482 - Initial plugin scaffolding.

---
+++ wp-graphql-file-upload.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Plugin Name:     WPGraphQL File Upload
+ * Plugin URI:      PLUGIN SITE HERE
+ * Description:     Add the ability to upload files through GraphQL to your WP GraphQL schema.
+ * Author:          Hughie Devore
+ * Author URI:      YOUR SITE HERE
+ * Text Domain:     wp-graphql-file-upload
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Wp_Graphql_File_Upload
+ */
