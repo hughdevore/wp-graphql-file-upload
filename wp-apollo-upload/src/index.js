35765d6c29e14f48d3c12e45c2627c8ca756c3f9
hughdevore <hughdevore@gmail.com>
2019-03-11 23:20:01 -0600

    CI-4482 - Add create react app and apollo upload.

---
+++ wp-apollo-upload/src/index.js
@@ -0,0 +1,66 @@
+import React from "react";
+import { render } from "react-dom";
+import ApolloClient from "apollo-boost";
+import { ApolloProvider } from "react-apollo";
+import { Mutation } from 'react-apollo';
+import gql from 'graphql-tag';
+import {
+  Upload, message, Button, Icon,
+} from 'antd';
+import './index.css';
+
+const client = new ApolloClient({
+  // Change this to the URL of your WordPress site.
+  uri: "http://denverpost.test/graphql"
+});
+
+const props = {
+  name: 'file',
+  action: '//jsonplaceholder.typicode.com/posts/',
+  headers: {
+    authorization: 'authorization-text',
+  },
+  onChange(info) {
+    if (info.file.status !== 'uploading') {
+      console.log(info.file, info.fileList);
+    }
+    if (info.file.status === 'done') {
+      message.success(`${info.file.name} file uploaded successfully`);
+    } else if (info.file.status === 'error') {
+      message.error(`${info.file.name} file upload failed.`);
+    }
+  },
+};
+
+// const UPLOAD_FILE_MUTATION = gql`
+//     mutation UPLOAD_FILE_MUTATION($input: CreateMediaItemInput! ) {
+//         createMediaItem(input: $input) {
+//             clientMutationId
+//             mediaItem {
+//                 id
+//                 mediaItemId
+//                 title
+//                 uri
+//                 sourceUrl
+//             }
+//         }
+//     }
+// `;
+
+const App = (props) => (
+  <ApolloProvider client={client}>
+    <div className="app">
+      <h2 className="title">
+        WP-GraphQL File Upload {' '}
+        <span className="emoji" role="img" aria-label="rocket">🚀</span>
+      </h2>
+      <Upload {...props}>
+        <Button>
+          <Icon type="upload" /> Click to Upload
+        </Button>
+      </Upload>
+    </div>
+  </ApolloProvider>
+);
+
+render(<App />, document.getElementById('root'));
--- wp-graphql-file-upload.php
+++ wp-graphql-file-upload.php
@@ -12,153 +12,81 @@
  * @package         WPGraphQL_File_Upload
  */
 namespace WPGraphQL\File_Upload;
+use ReactWPScripts;
 
-// If this file is called directly, abort.
-if ( ! defined( 'WPINC' ) ) {
-	die;
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
 }
 
-if ( ! class_exists( '\WPGraphQL\File_Upload' ) ) :
+if ( ! class_exists( 'WP_File_Upload' ) ) :
 
-	final class File_Upload {
+	include_once( dirname( __FILE__ ) . '/wp-apollo-upload/react-wp-scripts.php' );
 
-		/**
-		 * Stores the instance of the File_Upload class
-		 *
-		 * @var File_Upload The one true File_Upload
-		 * @since  0.0.1
-		 * @access private
-		 */
-		private static $instance;
+	class WP_File_Upload {
 
-		/**
-		 * The instance of the File_Upload object
-		 *
-		 * @return object|File_Upload - The one true File_Upload
-		 * @since  0.0.1
-		 * @access public
-		 */
-		public static function instance() {
+		public function init() {
 
-			if ( ! isset( self::$instance ) && ! ( self::$instance instanceof File_Upload ) ) {
-				self::$instance = new File_Upload;
-				self::$instance->setup_constants();
-				self::$instance->includes();
-			}
-
-			self::$instance->init();
-
-			/**
-			 * Fire off init action
-			 *
-			 * @param File_Upload $instance The instance of the Init_File_Upload class
-			 */
-			do_action( 'graphql_file_upload_init', self::$instance );
-
-			/**
-			 * Return the Init_File_Upload Instance
-			 */
-			return self::$instance;
-		}
-
-		/**
-		 * Throw error on object clone.
-		 * The whole idea of the singleton design pattern is that there is a single object
-		 * therefore, we don't want the object to be cloned.
-		 *
-		 * @since  0.0.1
-		 * @access public
-		 * @return void
-		 */
-		public function __clone() {
-
-			// Cloning instances of the class is forbidden.
-			_doing_it_wrong( __FUNCTION__, esc_html__( 'The Init_File_Upload class should not be cloned.', 'wp-graphql-file-upload' ), '0.0.1' );
-
-		}
-
-		/**
-		 * Disable de-serializing of the class.
-		 *
-		 * @since  0.0.1
-		 * @access protected
-		 * @return void
-		 */
-		public function __wakeup() {
-
-			// De-serializing instances of the class is forbidden.
-			_doing_it_wrong( __FUNCTION__, esc_html__( 'De-serializing instances of the WPGraphQL file upload class is not allowed.', 'wp-graphql-file-upload' ), '0.0.1' );
-
-		}
-
-		/**
-		 * Setup plugin constants.
-		 *
-		 * @access private
-		 * @since  0.0.1
-		 * @return void
-		 */
-		private function setup_constants() {
-
-			// Plugin version.
-			if ( ! defined( 'WPGRAPHQL_FILE_UPLOAD_VERSION' ) ) {
-				define( 'WPGRAPHQL_FILE_UPLOAD_VERSION', '0.3.1' );
-			}
 
 			// Plugin Folder Path.
 			if ( ! defined( 'WPGRAPHQL_FILE_UPLOAD_PLUGIN_DIR' ) ) {
-				define( 'WPGRAPHQL_FILE_UPLOAD_PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
+				define( 'WPGRAPHQL_FILE_UPLOAD_PLUGIN_DIR', plugin_dir_url( __FILE__ ) );
 			}
 
-			// Plugin Folder URL.
-			if ( ! defined( 'WPGRAPHQL_FILE_UPLOAD_PLUGIN_URL' ) ) {
-				define( 'WPGRAPHQL_FILE_UPLOAD_PLUGIN_URL', plugin_dir_url( __FILE__ ) );
-			}
+			add_action( 'admin_menu', [ $this, 'register_admin_page' ] );
+			add_action( 'admin_enqueue_scripts', [ $this, 'enqueue_react_app' ] );
+		}
 
-			// Plugin Root File.
-			if ( ! defined( 'WPGRAPHQL_FILE_UPLOAD_PLUGIN_FILE' ) ) {
-				define( 'WPGRAPHQL_FILE_UPLOAD_PLUGIN_FILE', __FILE__ );
-			}
+		public function is_wpgraphql_active() {
+			return class_exists( 'WPGraphQL' );
+		}
 
-			// Whether to autoload the files or not
-			if ( ! defined( 'WPGRAPHQL_FILE_UPLOAD_AUTOLOAD' ) ) {
-				define( 'WPGRAPHQL_FILE_UPLOAD_AUTOLOAD', true );
+		/*
+		 * Add a settings page for this plugin to the Settings menu.
+		 */
+		public function register_admin_page() {
+			add_menu_page(
+				__( 'WP Apollo Upload', 'wp-graphql-file-upload' ),
+				'WP Apollo Upload',
+				'manage_options',
+				'wp-apollo-upload',
+				[ $this, 'render_admin_page' ],
+				plugin_dir_url( __FILE__ . '/wp-apollo-upload/src/cloud-computing.svg' ),
+				1
+			);
+		}
+
+		public function render_admin_page() {
+
+			if ( $this->is_wpgraphql_active() ) {
+				echo '<div class="wrap"><div id="root"></div></div>';
+			} else {
+				echo '<div class="wrap"><h1>This plugin requires WPGraphQL to be installed to work. Please install WPGraphQL (https://github.com/wp-graphql/wp-graphql) and visit this page again.</h1></div>';
 			}
 
 		}
 
 		/**
-		 * Include required files.
-		 * Uses composer's autoload
-		 *
-		 * @access private
-		 * @since  0.0.1
-		 * @return void
+		 * Enqueues the stylesheet and js for the WPGraphiQL app
 		 */
-		private function includes() {
+		public function enqueue_react_app() {
 
-			// Autoload Required Classes
-			if ( defined( 'WPGRAPHQL_FILE_UPLOAD_AUTOLOAD' ) && true === WPGRAPHQL_FILE_UPLOAD_AUTOLOAD ) {
-				require_once( WPGRAPHQL_FILE_UPLOAD_PLUGIN_DIR . 'vendor/autoload.php' );
-			}
-		}
-
-		/**
-		 * Initialize the plugin
-		 */
-		private static function init() {
 			/**
-			 * Initialize the GraphQL fields for managing file uploads
+			 * Only enqueue the assets on the proper admin page, and only if WPGraphQL is also active
 			 */
-			ManageUploads::init();
+			if ( strpos( get_current_screen()->id, 'wp-apollo-upload' ) && $this->is_wpgraphql_active() ) {
+				ReactWPScripts\enqueue_assets( dirname( __FILE__ ) . '/wp-apollo-upload' );
+			}
+
 		}
 
 	}
 
-endif;
+endif; // End if class_exists()
 
-function init() {
-	return File_Upload::instance();
-}
-
-add_action( 'graphql_init', '\WPGraphQL\File_Upload\init' );
+add_action(
+	'plugins_loaded',
+	function() {
+		$wp_file_upload = new WP_File_Upload();
+		$wp_file_upload->init();
+	}
+);
